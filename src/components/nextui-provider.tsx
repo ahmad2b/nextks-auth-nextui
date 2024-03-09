46b0a9ae6f6121ef4dc6a29f10da07ad92396ded
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-09 13:33:03 +0500

    Initalized nextjs, nextui, clerk auth starter template

---
+++ src/components/nextui-provider.tsx
@@ -0,0 +1,8 @@
+// app/providers.tsx
+'use client';
+
+import { NextUIProvider as UIProvider } from '@nextui-org/react';
+
+export function NextUIProviders({ children }: { children: React.ReactNode }) {
+	return <UIProvider>{children}</UIProvider>;
+}
